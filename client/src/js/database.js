a73d99f93a50a03c52dc5eb96c927eaa9fc040cb
Brendan Munroe <munner7@gmail.com>
2022-07-27 23:21:59 -0400

    Final push

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,30 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const todosDb = await openDB('data', 1);
+  const tx = todosDb.transaction('data', 'readwrite');
+  const store = tx.objectStore('data');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
+
+export const getDb = async () => {
+  console.log('GET all from the database');
+  const todosDb = await openDB('data', 1);
+  const tx = todosDb.transaction('data', 'readonly');
+  const store = tx.objectStore('data');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  result
+  ? console.log('🚀 - data has been got', result.value)
+  : console.log('🚀 - data not found');
+  return result?.value
+}
+
 
 initdb();
